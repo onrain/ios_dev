6730d30ed1bd903d811193b56109a017d4cc9eff
onrain <prostirchas@gmail.com>
2012-09-17 10:31:56 +0300

    upd

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,28 +1,30 @@
 class AdminController < ApplicationController
-  def index
-   
-   # details list on admin index page
-   # Tree =>
-   # - Projects last 10
-   #   - Application of this proj(last 10) 
-   # - Client
-   # - Manager
-   # - Developers 
-   # DateS
+  respond_to :json, :xml, :html
+  helper_method :sort_column, :sort_direction
 
-   
-    @projects = Project.find_by_sql(
-    "select clients.name as client_name, managers.name as manager_name, applications.id as product_id,
-    projects.*
-    from projects
-    left join applications on applications.project_id = projects.id
+  def index  
+    project_size = Project.all.size
+    lim = params[:lim]
+    lim = project_size if params[:lim].eql? 'all'
+    @projects = Project
+    .select('clients.name as client_name, managers.name as manager_name, applications.id as product_id,
+    projects.*')
+    .joins('left join applications on applications.project_id = projects.id
     left join clients on clients.id = projects.client_id
-    left join managers on managers.id = projects.manager_id
-    " 
-    )
-
-
-
-
+    left join managers on managers.id = projects.manager_id')
+    .limit(lim).page(params[:page]).per(10).order(sort_column + " " + sort_direction)
+    
+    respond_with(@projects)
   end
+
+private
+  def sort_column
+    Client.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
+
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,5 @@ class Company < ActiveRecord::Base
   attr_accessible :name, :website
   validates :name, presence:true, uniqueness:true
   has_many :clients, :dependent=>:destroy
-  
-  
+   
 end
