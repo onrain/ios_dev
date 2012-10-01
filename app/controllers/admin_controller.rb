2f34c0e19400d2afd0d27aaeca47a01e1308b417
onrain <prostirchas@gmail.com>
2012-10-01 14:18:03 +0300

    upd

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,8 +11,8 @@ class AdminController < ApplicationController
     projects.*')
     .joins('left join applications on applications.project_id = projects.id
     left join clients on clients.id = projects.client_id
-    left join managers on managers.id = projects.manager_id
-    group by projects.id, clients.name,  managers.name')
+    left join managers on managers.id = projects.manager_id')
+    .group('projects.id, clients.name,  managers.name')
     .limit(lim).page(params[:page]).per(10).order(sort_column + " " + sort_direction)
     @new_app = Application.new
 
