06b87e7513a1cd03bda25373e2d619b606f5fb46
onrain <prostirchas@gmail.com>
2012-10-01 13:30:43 +0300

    upd

--- db/migrate/20120918112548_change_applications.rb
+++ db/migrate/20120918112548_change_applications.rb
@@ -1,6 +1,5 @@
 class ChangeApplications < ActiveRecord::Migration
   def change
-    change_column :applications, :bundle_identifier, :string
     change_column :applications, :bundle_version, :float, :default =>  1.0
   end
 end
