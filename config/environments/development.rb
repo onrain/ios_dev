b20e4fa0ccfdd171aec22a8314858a22125ded7e
onrain <prostirchas@gmail.com>
2012-10-16 13:54:34 +0300

    upd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,8 @@ IOSmanager::Application.configure do
   require 'active_support/core_ext'
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
+  config.action_controller.perform_caching = false
 
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
