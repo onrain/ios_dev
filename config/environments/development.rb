60aed8dc24ba1fdb90a9db2a59df4ba89c7e218f
onrain <prostirchas@gmail.com>
2012-10-16 10:13:15 +0300

    ipd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,6 @@ IOSmanager::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Don't care if the mailer can't send
